af8b432b4ebfbccf16403339e52ba783584944a1
thabatagomes <159185229+thabatagomes@users.noreply.github.com>
2024-10-01 16:40:31 -0300

    01-09

---
+++ assents/perguntas.js
@@ -0,0 +1,17 @@
+criarCartao(
+    'programacao',
+    'O que é Python', 
+    'O Python é uma linguguem de programacao',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programacao',
+    'O que é uma função',
+    'Uma funçao é um bloco de código que executa alguma tarefa'
+)
